3060cacb9e9434a16eaa46340bac47962f2a81f4
jpart-r <jpartdev@gmail.com>
2018-01-06 13:32:22 +0800

    Refactoring

--- src/app/utils.ts
+++
@@ -1,9 +0,0 @@
-export function format(...args: any[]): string {
-	let msg, params
-	(typeof arguments[0] === 'object') ? (msg = arguments[0][0], params = arguments[0]) : (msg = arguments[0], params = arguments)
-	msg = msg.replace(/{\d}/g, (s) => {
-		const key = (1 * s.match(/\d/)[0]) + 1
-		return typeof params[key] !== 'undefined' ? params[key] : s
-	})
-	return msg
-}
