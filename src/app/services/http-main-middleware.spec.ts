ff4e9f9aedfdfdbc317951c73fe031041171966b
jpart-r <jpartdev@gmail.com>
2018-03-07 16:26:02 +0800

    Add: mw specs

---
+++ src/app/services/http-main-middleware.spec.ts
@@ -0,0 +1,72 @@
+import { HttpClientTestingModule, HttpTestingController, TestRequest } from '@angular/common/http/testing'
+import { TestBed } from '@angular/core/testing'
+import { HttpClient, HttpErrorResponse, HTTP_INTERCEPTORS } from '@angular/common/http'
+import { HttpMainMiddleware } from './http-main-middleware'
+import { NotificationsService } from './notifications.service'
+import { format } from '../public/utils'
+
+describe('HttpMainMiddleware', () => {
+	let httpClient: HttpClient
+	let httpTestingController: HttpTestingController
+	let httpMiddleware: HttpMainMiddleware
+	let req: TestRequest
+	let nsSpyObj: jasmine.SpyObj<NotificationsService>
+	const testData: { name: string } = { name: 'Test' }, path = '/test'
+	beforeEach(() => {
+		nsSpyObj = jasmine.createSpyObj<NotificationsService>('NotificationsService', ['notify'])
+		httpMiddleware = new HttpMainMiddleware(nsSpyObj)
+		TestBed.configureTestingModule({
+			imports: [ 
+				HttpClientTestingModule,
+			], providers : [
+				{ provide: HTTP_INTERCEPTORS, useValue: httpMiddleware, multi: true }
+			]
+		})
+
+		httpClient = TestBed.get(HttpClient)
+		httpTestingController = TestBed.get(HttpTestingController)
+
+		spyOn<HttpMainMiddleware>(httpMiddleware, 'intercept').and.callThrough()
+		spyOn<HttpMainMiddleware>(httpMiddleware, 'logRequest').and.callThrough()
+	})
+
+	afterEach(() => {
+		httpTestingController.verify()
+	})
+
+	describe('a request that is NOT svg', () => {
+		beforeEach(() => {
+			httpClient.get<{ name: string }>(path).subscribe()
+			req = httpTestingController.expectOne(path)
+			req.flush(testData)
+		})
+
+		it('should call the intercept method', () => {
+			expect(httpMiddleware.intercept).toHaveBeenCalled()
+		})
+
+		it('should call the logRequest method', () => {
+			expect(httpMiddleware.logRequest).toHaveBeenCalled()
+		})
+
+		it(format('should call the logRequest method with {0},{1}', 'GET', path), () => {
+			expect(httpMiddleware.logRequest).toHaveBeenCalledWith(path, 'GET')
+		})
+
+		it('should call the ns notify method with info type', () => {
+			expect(nsSpyObj.notify.calls.argsFor(0)).toEqual(jasmine.arrayContaining(['info']))
+		})
+	})
+
+	describe('a request that is svg', () => {
+		beforeEach(() => {
+			httpClient.get<{ name: string }>('/assets/img.svg').subscribe()
+			req = httpTestingController.expectOne('/assets/img.svg')
+			req.flush(testData)
+		})
+
+		it('should NOT call the ns notify method', () => {
+			expect(nsSpyObj.notify).not.toHaveBeenCalled()
+		})
+	})
+})
--- src/app/services/http-main-middleware.ts
+++ src/app/services/http-main-middleware.ts
@@ -8,9 +8,14 @@ import { format } from '../public/utils'
 @Injectable()
 export class HttpMainMiddleware implements HttpInterceptor {
 	constructor(private notifications: NotificationsService) { }
-	intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-		// filter logging from calls to svg files - joey
-		req.url.indexOf('.svg') === -1 && this.notifications.notify('info', format('Http request to URL: {0}, method: {1}', req.url, req.method))
+	public intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+		// console.log('HttpMainMiddleware (intercept)')
+		this.logRequest(req.url, req.method)
 		return next.handle(req)
+	}
+
+	public logRequest(url: string, method: string): void {
+		// filter logging from calls to svg files
+		!url.match(/\.svg$/i) && this.notifications.notify('info', format('Http request to URL: {0}, method: {1}', url, method))
 	}
 }
