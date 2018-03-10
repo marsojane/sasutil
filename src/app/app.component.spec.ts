57ded58d4d2f85c04d920c65a2b5cd7d60c2d209
jpart-r <jpartdev@gmail.com>
2018-03-10 19:44:56 +0800

    add: app-component spec

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,55 @@
-import { TestBed } from '@angular/core/testing'
+import { NO_ERRORS_SCHEMA } from '@angular/core'
+import { TestBed, ComponentFixture } from '@angular/core/testing'
 import { AppComponent } from './app.component'
+import { Subject } from 'rxjs/Subject'
+import { Observable } from 'rxjs/Observable'
+import { flatten } from 'lodash'
+// providers
+import { RouterModule } from '@angular/router'
+import { MatIconRegistry } from '@angular/material'
+import { DomSanitizer } from '@angular/platform-browser'
+import { NotificationsService } from './services/notifications.service'
+import { Router } from '@angular/router'
+// import { AppRoutingModule } from './app-routing.module'
+import { MatImports } from './app.imports.material'
 
-xdescribe('AppComponent', () => {
-	//
+describe('AppComponent', () => {
+	let component: AppComponent
+	let fixture: ComponentFixture<AppComponent>
+	let matIconRegistrySpy: jasmine.SpyObj<MatIconRegistry>
+	let domSanitizerSpy: jasmine.SpyObj<DomSanitizer>
+	// let nsSpy: jasmine.SpyObj<NotificationsService>
+	// let routerSpy: jasmine.SpyObj<Router>
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			declarations: [ AppComponent ],
+			imports: flatten([RouterModule, MatImports]),
+			providers: [
+				{ provide: MatIconRegistry, useValue: jasmine.createSpyObj('MatIconRegistry', ['addSvgIcon']) },
+				{ provide: DomSanitizer, useValue: jasmine.createSpyObj('DomSanitizer', ['bypassSecurityTrustResourceUrl']) },
+				{ provide: NotificationsService, useValue: {} },
+				{ provide: Router, useValue: {} }
+			],
+			schemas: [ NO_ERRORS_SCHEMA ]
+		})
+		fixture = TestBed.createComponent(AppComponent)
+		component = fixture.componentInstance
+
+		matIconRegistrySpy = TestBed.get(MatIconRegistry)
+		domSanitizerSpy = TestBed.get(DomSanitizer)
+		// nsSpy = TestBed.get(NotificationsService)
+		// routerSpy = TestBed.get(Router)
+	})
+
+	it('should create AppComponent', () => {
+		expect(component).toBeDefined()
+	})
+
+	it('should call the addSvgIcon method thrice', () => {
+		expect(matIconRegistrySpy.addSvgIcon).toHaveBeenCalledTimes(3)
+	})
+
+	it('should call the bypassSecurityTrustResourceUrl method thrice', () => {
+		expect(domSanitizerSpy.bypassSecurityTrustResourceUrl).toHaveBeenCalledTimes(3)
+	})
 })
